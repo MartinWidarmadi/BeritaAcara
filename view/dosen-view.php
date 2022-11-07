12305da35e183275c1cd9ab86ae83125acba6fc1
MichaelMathew <2072007@maranatha.ac.id>
2022-11-07 23:29:19 +0700

    Update membuat dosen dapat melihat halaman dosen dan halaman mahasiswa
    Update membuat dosen dapat melihat halaman dosen dan halaman mahasiswa tetapi tidak dapat menambahkannya

--- view/dosen-view.php
+++ view/dosen-view.php
@@ -1,3 +1,31 @@
+<?php
+if ($_SESSION['roles'] == "dosen") :
+?>
+<table class="table" id="example">
+    <thead>
+    <tr>
+        <th scope="col">NIP</th>
+        <th scope="col">Dosen Name</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach($dosen as $dosens) {
+        echo '<tr>';
+        echo '<td>' . $dosens->getNIP() . '</td>';
+        echo '<td>' . $dosens->getNamaDosen() . '</td>';
+    }
+    ?>
+    </tbody>
+    <thead>
+    <tr></tr>
+    </thead>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.4/datatables.min.css" />
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.4/datatables.min.js"></script>
+</table>
+<?php
+else :
+?>
 <table class="table" id="example">
     <thead>
     <tr>
@@ -25,3 +53,5 @@
     <input type="submit" value="Add Dosen" name="btnSubmit" class="btn btn-primary">
 </div>
 </form>
+<?php
+endif; ?>
--- view/mahasiswa-view.php
+++ view/mahasiswa-view.php
@@ -1,3 +1,35 @@
+<?php
+if ($_SESSION['roles'] == "dosen") :
+?>
+<table class="table" id="example">
+    <thead>
+    <tr>
+        <th scope="col">NRP</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Alamat</th>
+        <th scope="col">No HP</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach($mahasiswa as $item) {
+        echo '<tr>';
+        echo '<td>' . $item->getNRP() . '</td>';
+        echo '<td>' . $item->getNama() . '</td>';
+        echo '<td>' . $item->getAlamat() . '</td>';
+        echo '<td>' . $item->getNoTlp() . '</td>';
+    }
+    ?>
+    </tbody>
+    <thead>
+    <tr></tr>
+    </thead>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.4/datatables.min.css" />
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.4/datatables.min.js"></script>
+</table>
+<?php
+else :
+?>
 <table class="table" id="example">
     <thead>
     <tr>
@@ -29,4 +61,5 @@
         <input type="submit" value="Add Mahasiswa" name="btnSubmit" class="btn btn-primary">
     </div>
 </form>
-
+<?php
+endif; ?>
