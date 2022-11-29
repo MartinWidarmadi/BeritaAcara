5ec175af4a6b74e167f80de186bfcdaec8639649
MichaelMathew <2072007@maranatha.ac.id>
2022-11-29 12:07:30 +0700

    menggunakan server database agar semua db anggota kelompok dapat langsung terupdate
    menggunakan server database agar semua db anggota kelompok dapat langsung terupdate

--- db-util/PDOUtil.php
+++ db-util/PDOUtil.php
@@ -1,7 +1,7 @@
 <?php 
 class PDOUtil {
   public static function connectDb () {
-    $link = new PDO('mysql:host=localhost;dbname=pbmdb', 'root', '');
+    $link = new PDO('mysql:host=18.143.100.149;dbname=pbmdb', 'admindbs', '');
     $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
     $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $link;
