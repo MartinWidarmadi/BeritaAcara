a9d7a8ec78d08955125dc052a4c6d2673fbb58ef
2072042MosesMarzukiSamosir <71442576+2072042MosesMarzukiSamosir@users.noreply.github.com>
2022-12-06 13:23:00 +0700

    update jadwal dao
    agarmenampilkan data dosen yg nonaktif

--- dao/JadwalDaoImpl.php
+++ dao/JadwalDaoImpl.php
@@ -14,7 +14,7 @@ class JadwalDaoImpl {
 
   public function fetchAllJadwals() {
     $link = PDOUtil::connectDb();
-    $query = 'SELECT jadwal.*,Dosen.NamaDosen AS "namadosen", matakuliah.NamaMataKuliah AS "matakuliah", matakuliah.idMataKuliah, Semester.nama_semester AS "semester" FROM jadwal JOIN dosen ON dosen.NIP = jadwal.Dosen_NIP JOIN matakuliah ON matakuliah.idMataKuliah = jadwal.MataKuliah_idMataKuliah JOIN Semester ON Semester.id_semester = jadwal.Semester_id_Semester WHERE Dosen.status != 0';
+    $query = 'SELECT jadwal.*,Dosen.NamaDosen AS "namadosen", matakuliah.NamaMataKuliah AS "matakuliah", matakuliah.idMataKuliah, Semester.nama_semester AS "semester" FROM jadwal JOIN dosen ON dosen.NIP = jadwal.Dosen_NIP JOIN matakuliah ON matakuliah.idMataKuliah = jadwal.MataKuliah_idMataKuliah JOIN Semester ON Semester.id_semester = jadwal.Semester_id_Semester';
     $stmt = $link->prepare($query);
     $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Jadwal');
     $stmt->execute();
